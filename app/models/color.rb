0341a37de44fddb1d36422a0bb6a71e2f30d4c5b
Jenny Veens <jennyveens@gmail.com>
2016-07-06 23:44:31 -0400

    Add user associations and palettes to nav

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,6 @@
 class Color < ActiveRecord::Base
 
+	belongs_to :user
 	has_many :palettes, :through => :palette_colours
 
 end
--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,5 +1,6 @@
 class Palette < ActiveRecord::Base
 
+	belongs_to :user
 	has_many :colors, :through => :palette_colours
 
 end
